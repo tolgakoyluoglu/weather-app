d3f94d5d8dabf8971287ad7c17059df25ad42e61
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-03-05 18:59:26 +0100

    Add som more styling, also added some more to the weather.js state from darksky API, like windspeed and humidity, next is to start working on my search form and finish getWeatherThenLocation function

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,6 +6,12 @@ export default class Weather extends Component {
 
     state = {
         weeklyWeather: [],
+        apparentTemperature: [],
+        icon: [],
+        temperature: [],
+        time: [],
+        humidity: [],
+        windSpeed: []
     }
 
     //Get weather based on geolocation
@@ -16,9 +22,16 @@ export default class Weather extends Component {
                     .then(res => {
                         console.log(res);
                         this.setState({
-                            weeklyWeather: res.data.daily.data
+                            weeklyWeather: res.data.daily.data,
+                            currentWeather: res.data.currently,
+                            apparentTemperature: res.data.currently.apparentTemperature,
+                            icon: res.data.currently.icon,
+                            temperature: res.data.currently.temperature,
+                            time: res.data.currently.time,
+                            humidity: res.data.currently.humidity,
+                            windSpeed: res.data.currently.windSpeed,
                         })
-                        console.log(res.data.daily.data)
+                        console.log(this.state)
                     })
             })
         }
@@ -41,6 +54,8 @@ export default class Weather extends Component {
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">Max: <strong>{weatherPerDay.temperatureMax}°C</strong></li>
                         <li className="list-group-item">Min: <strong> {weatherPerDay.temperatureMin}°C</strong></li>
+                        <li className="list-group-item">Sunrise:  {new Date(weatherPerDay.sunriseTime * 1000).toTimeString()}</li>
+                        <li className="list-group-item">Sundown:  {new Date(weatherPerDay.sunsetTime * 1000).toTimeString()}</li>
                     </ul>
                 </div>
             )
@@ -50,7 +65,6 @@ export default class Weather extends Component {
             <div className="container">
                 {weeklyWeatherList}
             </div>
-
         )
     }
 }
