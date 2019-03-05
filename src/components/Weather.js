1f73bb0527b8ed8fcd12b0a29d68cb8604ce2126
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-03-05 23:29:42 +0100

    Added a current weather card with info from currently that i saved in the state, added some styling to the card aswell. Next thing to do is to replace all the data from componentDidMount with the search input value when user search on a city

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -11,7 +11,8 @@ export default class Weather extends Component {
         temperature: [],
         time: [],
         humidity: [],
-        windSpeed: []
+        windSpeed: [],
+        summary: [],
     }
 
     //Get weather based on geolocation
@@ -23,15 +24,15 @@ export default class Weather extends Component {
                         console.log(res);
                         this.setState({
                             weeklyWeather: res.data.daily.data,
-                            currentWeather: res.data.currently,
                             apparentTemperature: res.data.currently.apparentTemperature,
                             icon: res.data.currently.icon,
                             temperature: res.data.currently.temperature,
                             time: res.data.currently.time,
                             humidity: res.data.currently.humidity,
                             windSpeed: res.data.currently.windSpeed,
+                            summary: res.data.currently.summary,
                         })
-                        console.log(this.state)
+                        //console.log(this.state)
                     })
             })
         }
@@ -41,19 +42,21 @@ export default class Weather extends Component {
     }
 
     render() {
+
+        //Stats of current weather 
         //loop thro sarch results and render
         const weeklyWeather = this.state.weeklyWeather
         const weeklyWeatherList = weeklyWeather.map(weatherPerDay => {
             return (
-                <div className="card" key={weatherPerDay.time}>
+                <div className="card-two" key={weatherPerDay.time}>
                     <div className="card-body">
                         <h5 className="card-title">{new Date(weatherPerDay.time * 1000).toDateString()} {weatherPerDay.city}</h5>
-                        <CurrentIcon currentIcon={weatherPerDay.icon} icon={weatherPerDay.icon} />
+                        <CurrentIcon icon={weatherPerDay.icon} />
                         <p className="card-text">{weatherPerDay.summary}</p>
                     </div>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Max: <strong>{weatherPerDay.temperatureMax}°C</strong></li>
-                        <li className="list-group-item">Min: <strong> {weatherPerDay.temperatureMin}°C</strong></li>
+                        <li className="list-group-item">Max: <strong>{weatherPerDay.temperatureMax}°</strong></li>
+                        <li className="list-group-item">Min: <strong> {weatherPerDay.temperatureMin}°</strong></li>
                         <li className="list-group-item">Sunrise:  {new Date(weatherPerDay.sunriseTime * 1000).toTimeString()}</li>
                         <li className="list-group-item">Sunset:  {new Date(weatherPerDay.sunsetTime * 1000).toTimeString()}</li>
                     </ul>
@@ -63,10 +66,34 @@ export default class Weather extends Component {
 
         return (
             <div className="container">
-                {weeklyWeatherList}
+                <h5>Current Weather</h5>
+                <div className="wrap">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{new Date(this.state.time * 1000).toDateString()}</h5>
+                            <p className="temp">{this.state.temperature}°</p>
+                            <div className="img-wrap">
+                                <CurrentIcon icon={this.state.icon} />
+                            </div>
+                            <p className="card-text">{this.state.summary}</p>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">Windspeed: {this.state.windSpeed}</li>
+                            <li className="list-group-item">Humidity: {this.state.humidity}</li>
+                            <li className="list-group-item">Temperature: {this.state.temperature}</li>
+                            <li className="list-group-item">Feels like: {this.state.apparentTemperature}</li>
+                        </ul>
+                    </div>
+                </div>
+                <h5>Weekly Weather</h5>
+                <div className="wrap-two">
+                    {weeklyWeatherList}
+                </div>
             </div>
         )
     }
 }
+
+
 
 
