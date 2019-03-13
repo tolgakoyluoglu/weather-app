8a6ce19e17fba19ce2fed3e297c01848d0a0bdfe
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-03-13 16:10:47 +0100

    Updates

---
+++ precache-manifest.f1322ba39428596d86a5fc9d37a8c04f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a86e0ddc240b6b2aa8b5323355e3e4aa",
+    "url": "/weather-app/static/media/partly-cloudy-day.a86e0ddc.svg"
+  },
+  {
+    "revision": "1de80e19e2ab18acffff",
+    "url": "/weather-app/static/css/main.cb5632ed.chunk.css"
+  },
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "f254c6492db35169f2f7",
+    "url": "/weather-app/static/js/2.f254c649.chunk.js"
+  },
+  {
+    "revision": "ba296eaa46105987653e3eacf1fa28d4",
+    "url": "/weather-app/static/media/clear-day.ba296eaa.svg"
+  },
+  {
+    "revision": "695e491991d9d5a094550e60d8145dde",
+    "url": "/weather-app/static/media/clear-night.695e4919.svg"
+  },
+  {
+    "revision": "9ac5c22f7fb71b7fee05554eff2ca5cf",
+    "url": "/weather-app/static/media/cloud.9ac5c22f.svg"
+  },
+  {
+    "revision": "859941bdb22dd7dc5c14cd3c86ded140",
+    "url": "/weather-app/static/media/fog.859941bd.svg"
+  },
+  {
+    "revision": "1de80e19e2ab18acffff",
+    "url": "/weather-app/static/js/main.1de80e19.chunk.js"
+  },
+  {
+    "revision": "d2bc0010430c830114925d639a0239e8",
+    "url": "/weather-app/static/media/partly-cloudy-night.d2bc0010.svg"
+  },
+  {
+    "revision": "b7a68b3316304572074df8c1001e5541",
+    "url": "/weather-app/static/media/rain.b7a68b33.svg"
+  },
+  {
+    "revision": "2a56fa8504bb16253a1cb4a848893f37",
+    "url": "/weather-app/static/media/sleet.2a56fa85.svg"
+  },
+  {
+    "revision": "a46524e64277dd82a8df9dc5978ddfc9",
+    "url": "/weather-app/static/media/snow.a46524e6.svg"
+  },
+  {
+    "revision": "4e68b29eb133cb9a22f6e9db4f303d4a",
+    "url": "/weather-app/static/media/wind.4e68b29e.svg"
+  },
+  {
+    "revision": "a3cc993b1f1d6de2e1a7754a4ebe8681",
+    "url": "/weather-app/static/media/loader.a3cc993b.svg"
+  },
+  {
+    "revision": "cab6b3ea6c61b90cbc0a75382ba837a2",
+    "url": "/weather-app/index.html"
+  }
+];
